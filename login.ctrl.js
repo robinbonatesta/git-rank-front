ac3253c83d9fb280a63c53784b6eb2d495987268
William <Euro_Driver@live.com>
2015-02-28 18:46:46 -0500

    There happy

---
+++ login.ctrl.js
@@ -0,0 +1,13 @@
+angular.module('app').controller("loginController",function(){
+var vm = this;
+vm.title = "Login";
+
+/* User name */
+vm.username = /*User*/
+vm.password = /*Pass*/
+
+
+
+
+
+});	
