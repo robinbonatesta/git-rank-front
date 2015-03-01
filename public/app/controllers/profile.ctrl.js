246ebc046d1562e738c393f10ff71f559b51ca28
Robin Bonatesta <robinbonatesta@gmail.com>
2015-02-28 21:19:59 -0600

    added correct links to groups page and fixed username problem

--- public/app/controllers/profile.ctrl.js
+++ public/app/controllers/profile.ctrl.js
@@ -1,22 +1,30 @@
-angular.module('app').controller("profileController", function($scope){
-    var vm = this;
-    vm.title = 'Profile';
-    
-    //get username
-    vm.name = localStorage.getItem('username');
-    console.log('name: ', vm.name);
+angular.module('app').controller("profileController", function($scope, $location){
+  var vm = this;
+  vm.title = 'Profile';
+
+  //Get username from path
+  var seg = $location.path().split('/');
+  var username = seg[2];
+  console.log(username);
   
-    vm.score = 102010101010;
-  
-    vm.groups=[
-      {
-        name:'hacksu',
-      },
-      {
-        name:'poopers',
-      },
-    ];
-        
+  //Storing the local Data.
+  localStorage.setItem('username', username);
+
+  //get username
+  vm.name = localStorage.getItem('username');
+  console.log('name: ', vm.name);
+
+  vm.score = 102010101010;
+
+  vm.groups=[
+    {
+      name:'hacksu',
+    },
+    {
+      name:'poopers',
+    },
+  ];
+
 });
 
 
